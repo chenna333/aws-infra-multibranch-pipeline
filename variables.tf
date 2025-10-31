edba11b35c7e11f41507f03c19a8a9796529cc99
chenna333 <59122294+chenna333@users.noreply.github.com>
2025-10-31 12:03:49 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,16 @@
-variable "aws_region" {
-  default = "us-east-1"
+variable "env" {
+  description = "Environment name (dev/qa/stg/mtf/prod)"
+  type        = string
 }
 
-variable "env" {}
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
 
 variable "cidr_block" {
-  default = "10.0.0.0/16"
+  description = "VPC CIDR"
+  type        = string
+  default     = "10.0.0.0/16"
 }
-
