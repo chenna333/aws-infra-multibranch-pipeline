2c2c24b09dd6f46e8be8efb2718c31066b004b1b
chenna333 <59122294+chenna333@users.noreply.github.com>
2025-10-31 12:08:45 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,15 +1,24 @@
 resource "aws_instance" "bastion" {
-  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2
+  ami           = data.aws_ami.amazon_linux.id
   instance_type = "t3.micro"
   subnet_id     = var.public_subnet_id
-  key_name      = "bastion-key"
+  key_name      = var.key_name
 
   tags = {
     Name = "${var.env}-bastion"
+    Env  = var.env
+  }
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
 }
 
 output "bastion_id" {
   value = aws_instance.bastion.id
 }
-
